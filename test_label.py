c076c97005ed090582e8f17859039bc11349361e
SFAS0 <sfas7@mail.ru>
2022-12-26 12:03:21 +0300

    начал работать над уровнями

--- test_label.py
+++ test_label.py
@@ -113,24 +113,6 @@ def draw_levels(screen):
     return location_labels_levels
 
 
-def level_selection(pos, loc):
-    global tick
-    global select_lavels
-    e_x, e_y, e_h, e_w = loc[0]
-    m_x, m_y, m_h, m_w = loc[1]
-    h_x, h_y, h_h, h_w = loc[2]
-    if e_x < pos[0] < e_x + e_w and e_y < pos[1] < e_y + e_h:
-        # ставим лёгкий уровень
-        print('esay')
-    elif m_x < pos[0] < m_x + m_w and m_y < pos[1] < m_y + m_h:
-        # ставим средний уровень
-        print('medium')
-    elif h_x < pos[0] < h_x + h_w and h_y < pos[1] < h_y + h_h:
-        # ставим средний уровень
-        print('hard')
-    tick = 60
-
-
 def clicking_on_the_level_label(pos, label_level):
     global lavels
     global select_lavels
@@ -154,8 +136,103 @@ ladders_sprites = pygame.sprite.Group()
 step = 10
 color_step = 15
 cloock = pygame.time.Clock()
+
+
+def load_level(filename):
+    filename = "data/" + filename
+    with open(filename, 'r') as mapFile:
+        level_map = [line.strip() for line in mapFile]
+    max_width = max(map(len, level_map))
+    return list(map(lambda x: list(x.ljust(max_width, '.')), level_map))
+
+
+tile_images = {
+    'wall': load_image('data/obstacles/pol3.png')
+}
+player_image = load_image('data/obstacles/avatar.png')
+tile_width = tile_height = 50
+
+
+class Ground(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y):
+        super().__init__(tiles_group, ground_sprites)
+        self.image = tile_images[tile_type]
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
+
+
+class Cube(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(player_group, player_group)
+        self.image = player_image
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x + 15, tile_height * pos_y + 5)
+        self.pos = pos_x, pos_y
+
+
+player = None
+
+
+ground_sprites = pygame.sprite.Group()
+tiles_group = pygame.sprite.Group()
+player_group = pygame.sprite.Group()
+
+
+def generate_level(level):
+    new_player, x, y = None, None, None
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            if level[y][x] == '0':
+                pass
+            elif level[y][x] == '1':
+                Ground('wall', x, y)
+            elif level[y][x] == '@':
+                new_player = Cube(x, y)
+                level[y][x] = '.'
+    return new_player, x, y
+
+
+class Camera:
+    def __init__(self):
+        self.dx = 0
+        self.dy = 0
+
+    def apply(self, obj):
+        obj.rect.x += self.dx
+        obj.rect.y += self.dy
+
+    def update(self, target):
+        self.dx = -(target.rect.x + target.rect.w // 2 - width // 2)
+        self.dy = -(target.rect.y + target.rect.h // 2 - height // 2)
+
+
+# camera = Camera()
+level_map = load_level('lvl1.txt')
+player, level_x, level_y = generate_level(level_map)
+
+
+def level_selection(pos, loc):
+    global tick
+    global select_lavels
+    global lvl_start
+    e_x, e_y, e_h, e_w = loc[0]
+    m_x, m_y, m_h, m_w = loc[1]
+    h_x, h_y, h_h, h_w = loc[2]
+    if e_x < pos[0] < e_x + e_w and e_y < pos[1] < e_y + e_h:
+        lvl_start = True
+        print('esay')
+    elif m_x < pos[0] < m_x + m_w and m_y < pos[1] < m_y + m_h:
+        # ставим средний уровень
+        print('medium')
+    elif h_x < pos[0] < h_x + h_w and h_y < pos[1] < h_y + h_h:
+        # ставим средний уровень
+        print('hard')
+    tick = 60
+
+
 running = True
 label_level = ''
+lvl_start = False
 while running:
     if add_text == 15:
         step_text -= 3
@@ -182,29 +259,16 @@ while running:
             if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 level_selection(event.pos, levels)
         else:
-            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 3:
-                pers_sprites = pygame.sprite.Group()
-                pers = Landing(event.pos)
             key = pygame.key.get_pressed()
-            if key[pygame.K_LEFT]:
-                pers.update(-step)
-            if key[pygame.K_RIGHT]:
-                pers.update(step)
-            if ((key[pygame.K_LCTRL] or key[pygame.K_RCTRL])
-                    and (event.type == pygame.MOUSEBUTTONDOWN and event.button == 1)):
-                Ladders(ladders_sprites, event.pos)
-            if (event.type == pygame.MOUSEBUTTONDOWN and
-                    event.button == 1 and not (key[pygame.K_LCTRL] or key[pygame.K_RCTRL])):
-                Platform(platforms_sprites, event.pos)
             if key[pygame.K_UP]:
-                pers.update(-step, True)
-            if key[pygame.K_DOWN]:
-                pers.update(step, True)
-    platforms_sprites.draw(screen)
-    platforms_sprites.update()
-    pers_sprites.draw(screen)
-    pers_sprites.update()
-    ladders_sprites.draw(screen)
-    ladders_sprites.update()
+                pass
+    # camera.update(player)
+    # for sprite in ground_sprites:
+    #     camera.apply(sprite)
+    if lvl_start:
+        tiles_group.draw(screen)
+        player_group.draw(screen)
+        platforms_sprites.draw(screen)
+        platforms_sprites.update()
     cloock.tick(tick)
     pygame.display.flip()
